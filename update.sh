85bad04d9b5372db11dd15dce4a60b0841b88c21
Orin Jaworski <orinj@chromium.org>
2021-10-07 15:19:42 -0700

    Update contents div in index.html

--- update.sh
+++ update.sh
@@ -6,5 +6,10 @@ pushd $CHROMIUM_SRC
 rm -rf ../doxygen_omnibox_out
 doxygen ../doxygen_omnibox
 popd
+echo "Existing snapshot/index.html --------------------"
+cat snapshot/index.html
+echo "-------------------------------------------------"
 rm -rf snapshot
 cp -r $CHROMIUM_SRC/../doxygen_omnibox_out/html ./snapshot
+
+echo "Don't forget to update snapshot/index.html contents div"
